b3d1c4c3528139d4f994290ed720f49e123a1ab7
perry <perry@channel.io>
2026-01-06 12:48:30 +0000

    style: Apply ESLint and Prettier fixes

--- apps/server/src/database/migrations/1767703577997-CreatePgvectorExtension.ts
+++ apps/server/src/database/migrations/1767703577997-CreatePgvectorExtension.ts
@@ -1,8 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreatePgvectorExtension1767703577997
-  implements MigrationInterface
-{
+export class CreatePgvectorExtension1767703577997 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS vector`);
   }
